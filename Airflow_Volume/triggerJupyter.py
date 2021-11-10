ac38731c8b29c6c712587f7468ebd931ffdfa2d9
Akira Chen <man4781747@gmail.com>
2021-11-10 09:03:14 +0000

    完成trigger Jupyter版本初版

---
+++ Airflow_Volume/triggerJupyter.py
@@ -0,0 +1,86 @@
+import json
+import requests
+import datetime
+import uuid
+import traceback
+from websocket import create_connection
+import re
+
+def send_execute_request(code):
+    msg_type = 'execute_request';
+    content = { 'code' : code, 'silent':False }
+    hdr = { 'msg_id' : uuid.uuid1().hex, 
+        'username': 'test', 
+        'session': uuid.uuid1().hex, 
+        'data': datetime.datetime.now().isoformat(),
+        'msg_type': msg_type,
+        'version' : '5.0' }
+    msg = { 'header': hdr, 'parent_header': hdr, 
+        'metadata': {},
+        'content': content }
+    return msg
+
+def run(S_jupyterNotebookUrl, S_jupyterToken):
+    Re_jupyterNotebookUrl = re.search(r"^(?P<jupyter_url>.*)/notebooks/(?P<notebook_path>.*)", S_jupyterNotebookUrl)
+    if not Re_jupyterNotebookUrl:
+        print('URL Format 錯誤')
+        print(S_jupyterNotebookUrl)
+        return None
+    
+
+    notebook_path = '/' + Re_jupyterNotebookUrl.group('notebook_path') 
+    base = Re_jupyterNotebookUrl.group('jupyter_url')
+    headers = {'Authorization': S_jupyterToken}
+    S_ip_port = base.split('//')[-1]
+    print(S_ip_port)
+    
+    url = base + '/api/sessions' + "?token={}".format(S_jupyterToken)
+
+    params = '{"path":\"%s\","type":"notebook","name":"","kernel":{"id":null,"name":"python3"}}' % notebook_path
+    response = requests.post(url, headers=headers, data=params)
+    session = json.loads(response.text)
+    kernel = session["kernel"]
+
+
+    # 讀取notebook檔案，並獲取每個Cell裡的Code
+    url = base + '/api/contents' + notebook_path + "?token={}".format(S_jupyterToken)
+    response = requests.get(url,headers=headers)
+    file = json.loads(response.text)
+    code = [ c['source'] for c in file['content']['cells'] if len(c['source'])>0 ]    
+        
+    # 開始啟動 WebSocket channels (request/reply)
+    S_ws_url = "ws://{}/api/kernels/".format(S_ip_port)+kernel["id"]+"/channels?session_id"+session["id"] + "&token={}".format(S_jupyterToken)
+    ws = create_connection(
+        S_ws_url, 
+        header=headers)
+    for c in code:
+        ws.send(json.dumps(send_execute_request(c)))
+        
+    # 我們只拿Code執行完的訊息結果，其他訊息將被忽略
+    for i in range(0, len(code)+1):
+        try:
+            msg_type = ''
+            while True:
+                rsp = json.loads(ws.recv())
+                msg_type = rsp["msg_type"]
+                if msg_type == "stream":
+                    print(rsp["content"]["text"])
+                elif msg_type == "execute_result":
+                    if "image/png" in (rsp["content"]["data"].keys()):
+                        print(rsp["content"]["data"]["image/png"])
+                    else:
+                        print(rsp["content"]["data"]["text/plain"])
+                elif msg_type == "display_data":
+                    print(rsp["content"]["data"]["image/png"])
+                elif msg_type == "error":
+                    print(rsp["content"]["traceback"])
+                elif msg_type == "status" and rsp["content"]["execution_state"] == "idle":
+                    break
+        except:
+                traceback.print_exc()
+                ws.close()
+                
+    ws.close()
+    
+if __name__=='__main__':
+    run("http://35.194.167.48:8001/notebooks/TestFloder/TestPyFile.ipynb","password")
